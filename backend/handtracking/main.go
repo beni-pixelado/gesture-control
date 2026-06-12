1cc328dd7d014974efef0475c5f9f8f1d6563b03
beni-pixelado <beni.fantinato26@gmail.com>
2026-06-12 21:01:03 +0000

    WIP neon auth

--- backend/handtracking/main.go
+++ backend/handtracking/main.go
@@ -13,40 +13,48 @@ func main() {
 
 	database.Init()
 
-	router := gin.Default()
+	r := gin.Default()
 
 	_, currentFile, _, _ := runtime.Caller(0)
-	
+
 	mainDir := filepath.Dir(currentFile)
 
 	templatesPath := filepath.Join(mainDir, "templates", "*")
 
 	staticPath := filepath.Join(mainDir, "..", "..", "frontend", "static")
 
-	
-	router.LoadHTMLGlob(templatesPath)
+	r.LoadHTMLGlob(templatesPath)
 
-	router.Static("/static", staticPath)
+	r.Static("/static", staticPath)
 
-	
-	router.GET("/", func(c *gin.Context) {
+	r.Static("/assets", "./neon-login/dist/assets")
+
+	r.GET("/", func(c *gin.Context) {
+    c.File("./backend/neon-login/index.html")
+})
+
+// fallback (IMPORTANTE)
+r.NoRoute(func(c *gin.Context) {
+    c.File("./backend/neon-login/dist/index.html")
+})
+
+	r.GET("/hub", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	router.GET("/PDA", func(c *gin.Context) {
+	r.GET("/PDA", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "PDA.html", nil)
 	})
 
-	router.GET("/page-b", func(c *gin.Context) {
+	r.GET("/page-b", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "page-b.html", nil)
 	})
 
-	router.GET("/tracker", func(c *gin.Context) {
+	r.GET("/tracker", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "tracker.html", nil)
 	})
 
-
-	if err := router.Run(":8000"); err != nil {
+	if err := r.Run(":8000"); err != nil {
 		panic(err)
 	}
 
